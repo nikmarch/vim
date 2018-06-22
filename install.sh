cb4bda5c9db1dbdd83d8ca23647dde4ca60863fc
Nik Marchenko <nikolay.marchenko@dollarshaveclub.com>
2018-06-21 22:57:45 -0700

    Add OS recogniton for ctags installation

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-brew reinstall ctags
-brew reinstall vim
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  sudo apt-get update
+  sudo apt-get install exuberant-ctags
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  brew reinstall ctags
+  brew reinstall vim
+fi
+
 mkdir ~/.vim
 cp -R dotvim/* ~/.vim/
 
