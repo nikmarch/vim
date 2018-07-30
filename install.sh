e7de95fb1600ec97ab467184faf1d1cde8bec4f3
Nik Marchenko <nikolay.marchenko@dollarshaveclub.com>
2018-07-30 11:11:44 -0700

    Minior install fix

--- install.sh
+++ install.sh
@@ -15,6 +15,6 @@ sudo cp vimrc.before ~/.vimrc.before
 sudo cp ctags ~/.ctags
 sudo git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 sudo git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
-sudo git clone git://github.com/tpope/vim-rsi.git ~/.vim/bundle/
-sudo git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/
-sudo git clone git://github.com/godlygeek/tabular.git ~/.vim/bundle/
+sudo git clone git://github.com/tpope/vim-rsi.git ~/.vim/bundle/vim-rsi
+sudo git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
+sudo git clone git://github.com/godlygeek/tabular.git ~/.vim/bundle/tabular
