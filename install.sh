7e61297b979e7ec35fc37b0354c11261ee22a87f
Nik <nick.a.march@gmail.com>
2018-07-29 22:15:23 -0700

    Awesome plugin for shell jump commands

--- install.sh
+++ install.sh
@@ -15,3 +15,4 @@ sudo cp vimrc.before ~/.vimrc.before
 sudo cp ctags ~/.ctags
 sudo git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 sudo git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
+sudo git clone git://github.com/tpope/vim-rsi.git ~/.vim/bundle/
