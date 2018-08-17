624320af01da9c07de5b3680c2af70b48fd09159
Nik <nick.a.march@gmail.com>
2018-08-16 21:54:03 -0700

    Remove unneeded sudo

--- plugins-install.sh
+++ plugins-install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-sudo mkdir ~/.vim
-sudo cp -R vim/* ~/.vim/
-sudo cp vimrc ~/.vimrc
-sudo cp vimrc.before ~/.vimrc.before
-sudo cp ctags ~/.ctags
-sudo git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
-sudo git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
-sudo git clone git://github.com/tpope/vim-rsi.git ~/.vim/bundle/vim-rsi
-sudo git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
-sudo git clone git://github.com/godlygeek/tabular.git ~/.vim/bundle/tabular
-sudo git clone https://github.com/tpope/vim-endwise.git ~/.vim/bundle/vim-endwise
+mkdir ~/.vim
+cp -R vim/* ~/.vim/
+cp vimrc ~/.vimrc
+cp vimrc.before ~/.vimrc.before
+cp ctags ~/.ctags
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
+git clone git://github.com/tpope/vim-rsi.git ~/.vim/bundle/vim-rsi
+git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
+git clone git://github.com/godlygeek/tabular.git ~/.vim/bundle/tabular
+git clone https://github.com/tpope/vim-endwise.git ~/.vim/bundle/vim-endwise
 
 mkdir -p ~/.vim/pack/tpope/start
 cd ~/.vim/pack/tpope/start
