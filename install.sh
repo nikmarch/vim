16df83c4fc389b93f6f8c81d4109d6fd04b6f961
Nik <nick.a.march@gmail.com>
2018-12-27 23:03:06 -0800

    Should be it

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
-!#/bin/bash
-
 #!/bin/bash
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
@@ -7,12 +5,33 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
   sudo apt-get install exuberant-ctags
   sudo apt-get install vim
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  brew update
-  brew reinstall ctags
-  brew reinstall vim
+#  brew update
+#  brew reinstall ctags
+#  brew reinstall vim
+echo "Breeeeewwwww"
 fi
 
-mkdir ~/.vim/backups
+mkdir -p ~/.vim/backups
 cp vimrc ~/.vimrc
 cp -R  vim ~/.vim
 
+# install plugins
+git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+git clone https://github.com/ervandew/supertab.git ~/.vim/bundle/supertab
+git clone git://github.com/godlygeek/tabular.git ~/.vim/bundle/tabular
+git clone https://github.com/vim-airline/vim-airline.git ~/.vim/bundle/vim-airline
+git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
+git clone https://github.com/majutsushi/tagbar.git ~/.vim/bundle/tagbar
+git clone git clone git://github.com/tpope/vim-endwise.git ~/.vim/bundle/vim-endwise
+git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
+# vim -u NONE -c "helptags vim-fugitive/doc" -c q
+git clone https://tpope.io/vim/sensible.git ~/.vim/bundle/sensible
+# vim -u NONE -c "helptags sensible/doc" -c q
+mkdir -p ~/.vim/pack/tpope/start
+cd ~/.vim/pack/tpope/start
+git clone https://tpope.io/vim/dispatch.git
+# vim -u NONE -c "helptags dispatch/doc" -c q
+cd -
+git clone https://github.com/tpope/vim-rails.git ~/.vim/bundle/vim-rails
+# vim -u NONE -c "helptags vim-rails/doc" -c q
