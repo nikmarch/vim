fb155d8cde0eb5ec97ee96415ea928db9a1ad6bb
Nik <nick.a.march@gmail.com>
2019-04-06 23:21:30 -0700

    add tmux

--- install.sh
+++ install.sh
@@ -4,14 +4,17 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
   sudo apt-get update
   sudo apt-get install exuberant-ctags
   sudo apt-get install vim
+  sudo apt-get install tmux
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   brew update
   brew reinstall ctags
   brew reinstall vim
   npm install -g git+https://github.com/ramitos/jsctags.git
+  brew install tmux
 fi
 
 cp -R vim ~/.vim
+cp tmux.conf ~/.tmux.conf
 mkdir -p ~/.vim/backups
 cp vimrc ~/.vimrc
 
